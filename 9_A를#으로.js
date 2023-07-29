fbacb5ad70445f74933237ebb84aa5dbf99de3f7
KYW <qodna25@gmail.com>
2023-07-29 23:00:30 +0900

    9. A를 #으로

---
+++ 9_A를#으로.js
@@ -0,0 +1,29 @@
+/* function solution(s) {
+  let answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "A") {
+      answer = s.replaceAll(s[i], "#");
+    }
+  }
+
+  return answer;
+}
+
+let str = "BANANA";
+console.log(solution(str)); */
+
+
+
+/* function solution(s) {
+    let answer =s.replaceAll('A','#')
+    return answer;
+  }
+  
+  let str = "BANANA";
+  console.log(solution(str));
+  
+ */
+
+
+  
